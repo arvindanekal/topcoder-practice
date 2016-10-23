e7677de075cde99034e3e7f3de83fe63d6f44289
Arvind Ranganathan <arvindanekal@users.noreply.github.com>
2016-10-23 14:47:40 -0500

    Create ChessMetric.java

---
+++ ChessMetric.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChessMetric {
+
+	public static void main(String args[]){
+		System.out.println(
+				new ChessMetric().howMany(100,
+		new int[]{0,0},
+		new int[]{0,99},
+		50
+
+
+		));
+	}
+
+	public 	long howMany(int size, int[] start, int[] end, int numMoves){
+		long[][][] matrix = new long[size][size][numMoves+1];
+		matrix[start[0]][start[1]][0] = 1;
+
+		for(int k = 1;k<=numMoves;k++){
+			for(int i = 0;i<size;i++)
+				for(int j = 0;j<size;j++){
+						int x = i;
+						int y = j;
+						checkValid(x, y, x+1, y, size, matrix, k);
+						checkValid(x, y, x-1, y, size, matrix, k);
+						checkValid(x, y, x, y+1, size, matrix, k);
+						checkValid(x, y, x+1, y+1, size, matrix, k);
+						checkValid(x, y, x-1, y+1, size, matrix, k);
+						checkValid(x, y, x, y-1, size, matrix, k);
+						checkValid(x, y, x+1, y-1, size, matrix, k);
+						checkValid(x, y, x-1, y-1, size, matrix, k);
+
+						// knight moves
+						checkValid(x, y, x+1, y+2, size, matrix, k);
+						checkValid(x, y, x+2, y+1, size, matrix, k);
+						checkValid(x, y, x+2, y-1, size, matrix, k);
+						checkValid(x, y, x+1, y-2, size, matrix, k);
+						checkValid(x, y, x-1, y-2, size, matrix, k);
+						checkValid(x, y, x-2, y-1, size, matrix, k);
+						checkValid(x, y, x-2, y+1, size, matrix, k);
+						checkValid(x, y, x-1, y+2, size, matrix, k);
+					}
+				}
+		return matrix[end[0]][end[1]][numMoves];
+	}
+
+	public boolean checkValid(int i, int j, int x, int y, int size, long[][][] matrix, int level){
+		if(x < 0 || y < 0 || x >= size || y >= size) return false;
+		matrix[x][y][level] += matrix[i][j][level-1];
+		return true;
+	}
+}
